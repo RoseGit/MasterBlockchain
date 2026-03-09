8f21205dca8aba7b22938bc76056f883e36bda79
RoseGit <ing.rosalio.garcia.cruz@gmail.com>
2026-03-09 11:08:42 -0600

    Parametros API Rest

--- FormacionBloque2/PracticeRustWeb/rocket-practice/src/main.rs
+++ FormacionBloque2/PracticeRustWeb/rocket-practice/src/main.rs
@@ -1,3 +1,5 @@
+use rocket::futures::io::ReadToString;
+
 #[macro_use] extern crate rocket;
 
 // Definimos la ruta GET básica
@@ -11,13 +13,20 @@ fn hello() -> &'static str{
     "Hello from /hello endpoint "
 }
 
+// Nueva ruta que recibe un parámetro 'id'
+// El tipo 'i32' asegura que solo acepte números enteros
+#[get("/item/<id>/nombre/<name>")]
+fn get_item(id: i32, name:String) -> String {
+    format!("Has solicitado el ítem con el ID: {} and name: {}", id, name)
+}
+
 // Punto de entrada principal
 // #[launch] levanta el servidor como tal 
 // routes![index] index apunta realmente a la funcion fn index() -> &'static str 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index, hello]) // Registramos las rutas
+        .mount("/", routes![index, hello, get_item]) // Registramos las rutas
         //.mount("/api", routes![helloApi]) // Registramos las rutas con contexto /api
 }
 
